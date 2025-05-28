779a1d9dea843d261e6fe1e8419bc491e9f010eb
mah131 <maisapinheiro2007@gmail.com>
2025-05-28 09:17:05 -0300

    Add files via upload
    Signed-off-by: mah131 <maisapinheiro2007@gmail.com>

---
+++ exercicio7_trocadevalores.py
@@ -0,0 +1,12 @@
+valor1 = input("Digite o primeiro valor: ")
+valor2 = input("Digite o segundo valor: ")
+
+print("\nAntes da troca:")
+print("valor1:", valor1)
+print("valor2:", valor2)
+
+valor1, valor2 = valor2, valor1
+
+print("\nDepois da troca:")
+print("valor1:", valor1)
+print("valor2", valor2)
