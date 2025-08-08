5020cd2b0093b8b9d0a3d6906ad8f186661c3a8e
mah131 <maisapinheiro2007@gmail.com>
2025-08-08 10:57:33 -0300

    Add files via upload
    Signed-off-by: mah131 <maisapinheiro2007@gmail.com>

---
+++ script.js
@@ -0,0 +1,4 @@
+function mudarMensagem() {
+    const men = document.getElementById("mensagem");
+    men.innerHTML = "Você acabou de GANHAR 5% de descontos em todos os produtos. <br> Aproveite!!!!!";
+}
